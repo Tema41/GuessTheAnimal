b6993cefe45a12a8cd5b6a1151e08ebb28555cb0
Artem41 <zykov1979@bk.ru>
2022-09-08 15:14:45 +0300

    Угадай животное:)

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+^
+.src/com/guesstheanimal/GuessTheAnimalGame.java,d\f\dff6048fe2fc0f60f6c3b445c71e45971c02d26a
---
+++ src/com/guesstheanimal/Play.java
@@ -0,0 +1,49 @@
+package com.guesstheanimal;
+
+import java.util.Map;
+
+import static com.guesstheanimal.GuessTheAnimalGame.generator;
+import static com.guesstheanimal.GuessTheAnimalGame.scanner;
+
+public class Play {
+    static void startGame(Map<String, String> animals) {
+        boolean continueGame = true;
+        while (continueGame) {
+            System.out.println("Загадай животное, а я попробую угадать...");
+            Object[] computerAnimal = animals.keySet().toArray();
+            String randomValue = (String) computerAnimal[generator.nextInt(computerAnimal.length)];
+            System.out.println("Это животное " + animals.get(randomValue.toLowerCase()) + "? (да/нет)");
+            String userResponse = scanner.nextLine();
+
+            guessTheAnimal(animals, userResponse, randomValue);
+
+            System.out.println("Хочешь продолжить игру? (да/нет)");
+            String response = scanner.nextLine();
+            continueGame = response.equalsIgnoreCase("да");
+        }
+    }
+
+    private static void guessTheAnimal(Map<String, String> animals, String userResponse, String randomValue) {
+        if (userResponse.equalsIgnoreCase("да")) {
+            System.out.println("Это животное '" + randomValue + "'?");
+            String userResponse2 = scanner.nextLine();
+            if (userResponse2.equalsIgnoreCase("да")) {
+                System.out.println("Я победил!");
+            } else {
+                addNewAnimal(randomValue, animals);
+            }
+        } else {
+            addNewAnimal(randomValue, animals);
+        }
+    }
+
+    private static void addNewAnimal(String newAnimal, Map<String, String> animals) {
+        System.out.println("Какое животное ты загадал?");
+        String randomValue = scanner.nextLine();
+        System.out.println("Чем <" + randomValue + "> отличается от <" + newAnimal + ">?");
+        String newValue = scanner.nextLine();
+        animals.put(randomValue, newValue);
+        System.out.println("Теперь я знаю таких животных --> " + animals);
+
+    }
+}
